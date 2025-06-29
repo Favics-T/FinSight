7873cb3791c34ddb35fb917de50ea3e9c054a7be
unknown <taiwokolapo533@gmail.com>
2025-06-29 16:23:20 +0100

    fixing vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
 import DashBoard from './pages/DashBoard';
 import Market from './pages/Market';
